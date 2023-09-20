4dbbbf1d46919627c18e068a53562c35bacbd614
feliperjj <felipeoliveirabento@gmail.com>
2023-09-19 23:28:31 -0300

    Atualizaçoes

--- modules/pokemon.js
+++ modules/pokemon.js
@@ -1,25 +1,41 @@
 export default function carrinho() {
-  
-  const pegaCarrinhos = document.querySelectorAll("#carrinho");
+  const produtos = document.querySelectorAll(".itemComprar");
+  const carrinhoItens = document.getElementById("carrinho");
+  const itensNoCarrinho = [];
 
+  produtos.forEach((produto) => {
+    produto.addEventListener('click', () => {
+      const nomeProduto = produto.textContent;
+      itensNoCarrinho.push(nomeProduto);
+      carrinhoItens.innerHTML = '';
 
+      itensNoCarrinho.forEach((nomeItem) => {
+        const itemCarrinho = document.createElement('div');
+        itemCarrinho.textContent = nomeItem;
+        carrinhoItens.appendChild(itemCarrinho);
+      });
 
-
-// card.forEach(cards => {
-// cards.addEventListener('mouseover',()=>{
-// console.log(card);
-
-// })
-
-// })
-
-
-pegaCarrinhos.forEach(carrinho => {
-  const cardId =document.getElementById(".carrinhocard");
-  carrinho.addEventListener('click', () => {
+      const carrinhoJSON = JSON.stringify(itensNoCarrinho);
+      localStorage.setItem("carrinho", carrinhoJSON);
+    });
     
   });
-});
+   function clearStorage (){
+   
+    console.log("o Storage foi Limpo");
+    localStorage.clear(); 
+   }
+   setTimeout(clearStorage,30000);
+   
+
 }
+// pegaCarrinhos.forEach(carrinho => {
+//   // const cardId =document.getElementById(".carrinhocard");
+  
+//   carrinho.addEventListener('click', () => {
+//     arrayConteudo.push;
+//   });
+// });
+// }
 
   
