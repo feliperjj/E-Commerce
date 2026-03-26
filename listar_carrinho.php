e877f570e8b7e764917b9b5f2e9250ef1353696c
feliperjj <felipeoliveirabento@gmail.com>
2026-03-26 16:15:18 -0300

    Update .gitignore to include database and temporary files; refactor listar_carrinho.php to simplify user retrieval logic

--- listar_carrinho.php
+++ listar_carrinho.php
@@ -6,15 +6,14 @@ session_start();
 require_once 'db_config.php';
 header('Content-Type: application/json');
 
-if (isset($_SESSION['username'])) {
-    $usuario =$_SESSION['username'];
-    try {
-        $stmt = $pdo->prepare('SELECT * FROM carrinho WHERE usuario = :usuario');
-        $stmt->execute([':username' => $usuario]);
-        $itens = $stmt->fetchAll();
-        echo json_encode($itens);
-    } catch (PDOException $e) {
-        echo json_encode(['erro' => $e->getMessage()]);
-    }
+$usuario = isset($_GET['usuario']) ? $_GET['usuario'] : 'visitante';
+
+try {
+    $stmt = $pdo->prepare('SELECT * FROM carrinho WHERE usuario = :usuario');
+    $stmt->execute([':usuario' => $usuario]);
+    $itens = $stmt->fetchAll();
+    echo json_encode($itens);
+} catch (PDOException $e) {
+    echo json_encode(['erro' => $e->getMessage()]);
 }
 ?>
