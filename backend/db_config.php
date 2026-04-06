60cac353bf020bb2f556c44e492b1e7b70c3942b
Felipe Bento <81867174+feliperjj@users.noreply.github.com>
2026-04-06 12:51:41 -0300

    Update db_config.php

--- backend/db_config.php
+++ backend/db_config.php
@@ -1,17 +1,17 @@
 <?php
 // public/api/db_config.php
 
-$host   = 'sql202.infinityfree.com'; // Corrigido conforme seu print
-$dbname = 'if0_41592805_loja';       // Corrigido: Usuário + Nome que você criou
-$user   = 'if0_41592805';            // Corrigido conforme seu print
-$pass   = 'sucodefruta97';           // Senha correta
+$host   = 'seu_host'; 
+$dbname = 'seu_db';      //Usuário + Nome que você criou
+$user   = 'seu_user';            
+$pass   = 'sua_senha';         
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 } catch (PDOException $e) {
-    // Se der erro, ele cospe o JSON pra gente debugar
+   
     header('Content-Type: application/json');
     die(json_encode(["sucesso" => false, "erro" => "Erro de conexao: " . $e->getMessage()]));
 }
